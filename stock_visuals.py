68f1b7255a23bcb73efe7eec5bc6af1974505a1b
structur <childeatsdog@gmail.com>
2014-11-27 14:21:00 -0500

    Added some commends and a docstring (tentative) and cleared it with Pep8

--- stock_visuals.py
+++ stock_visuals.py
@@ -1,21 +1,26 @@
 __author__ = 'curtismccord'
 
 #!/usr/bin/env python
-# a bar plot with errorbars
+"""
+Makes a bar graph out of imported lists of stock tuples
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Import jsons and functions OR function results
+# Import jsons and functions OR function results OR the classes...
 
 
-six_best_months = [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42), ('2008/05', 576.29), ('2008/06', 555.34)]
+six_best_months = [('2007/12', 693.76), ('2007/11', 676.55), ('2007/10', 637.38), ('2008/01', 599.42),
+                   ('2008/05', 576.29), ('2008/06', 555.34)]
 
-six_worst_months = [('2004/08', 104.66), ('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09), ('2004/12', 181.01), ('2005/03', 181.18)]
+six_worst_months = [('2004/08', 104.66), ('2004/09', 116.38), ('2004/10', 164.52), ('2004/11', 177.09),
+                    ('2004/12', 181.01), ('2005/03', 181.18)]
 
 best_stock_prices = []
 best_stock_dates = []
 worst_stock_prices = []
 worst_stock_dates = []
+
 
 def sort_stock_tuples(stock_result):
     """
@@ -34,29 +39,45 @@ sort_stock_tuples(six_best_months)
 sort_stock_tuples(six_worst_months)
 
 N = 6
-
 ind = np.arange(N)  # the x locations for the groups
-width = 0.45       # the width of the bars
-
+width = 0.5       # sets the width of the bars
+# sets two subplot objects to share a y axis
 fig, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
+# names the subplots and sets their parametres
 prices1 = ax1.bar(ind, best_stock_prices, width, color='#99CC00', align='center')
-
 prices2 = ax2.bar(ind, worst_stock_prices, width, color='#FF0066', align='center')
 
-# add some text for labels, title and axes ticks
+# sets the titles for each subplots
 ax1.set_title('stock_name\n best average prices')
 ax2.set_title('stock_name\n worst average prices')
+
+# sets ticks along the x-axis
 ax1.set_xticks(ind)
 ax2.set_xticks(ind)
+
+# makes the inside borders invisible
 ax2.spines["left"].set_visible(False)
 ax1.spines["right"].set_visible(False)
-ax1.set_xticklabels( (best_stock_dates), rotation='90')
-ax2.set_xticklabels( (worst_stock_dates), rotation='90')
+
+# sets the x-axis labels using the variables generated above and makes them vertical
+ax1.set_xticklabels((best_stock_dates), rotation='90')
+ax2.set_xticklabels((worst_stock_dates), rotation='90')
+
+# puts a dotted line grid over the subplots to improve visibility
 ax1.yaxis.grid(b=True, linestyle="--")
 ax2.yaxis.grid(b=True, linestyle="--")
-ax1.set_ylabel('$ USD')
 
+# sets the y- axis label... with subtle LaTeX!
+ax1.set_ylabel(r'$\$$ USD')
+
+# tightens and aligns formatting
 plt.tight_layout()
+
+# makes the plot interactive
+# plt.ion()
+
+# saves a .png file in the directory
 fig.savefig('stock_bar.png', transparent=True)
+
 plt.show()
