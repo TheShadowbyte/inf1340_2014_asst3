d7126940beefb14e903075a26eb009b9f79fa30c
= <jordanov@mail.utoronto.ca>
2014-11-27 23:28:56 -0500

    Cleanup + attempt at instantiating object from remote class.

--- bonus_01_compare.py
+++ bonus_01_compare.py
@@ -3,50 +3,71 @@ __author__ = 'Dimitar'
 import math
 from mining import *
 
-stock1 = StockMiner("stock1", "data/GOOG.json")
-stock2 = StockMiner("stock2", "data/GOOG.json")
+#stock1 = StockMiner("first_stock", "data/GOOG.json")
+#stock2 = StockMiner("second_stock", "data/GOOG.json")
+
+first_stock_object = StockMiner
+first_stock = first_stock_object.get_monthly_averages_list(self)
+
+second_stock_object = StockMiner
+second_stock = second_stock_object.get_monthly_averages_list(self)
 
 
-def compare_stocks(stock1, stock2):
+def compare_two_stocks(stock1, stock2):
+
     stock1_mean_sum = 0
-    stock2_mean_sum = 0
     stock1_counter = 0
-    stock2_counter = 0
     squared_values_list1_mean = 0
-    squared_values_list2_mean = 0
     squared_values_list1 = []
+
+    stock2_mean_sum = 0
+    stock2_counter = 0
+    squared_values_list2_mean = 0
     squared_values_list2 = []
+
     for entry in stock1:
         stock1_mean_sum += entry
         stock1_counter += 1
+
     stock1_mean = stock1_mean_sum / stock1_counter
-    for entry in stock2:
-        stock2_mean_sum += entry
-        stock2_counter += 1
-    stock2_mean = stock2_mean_sum / stock2_counter
+
     for value in stock1:
         value_squared1 = (value - stock1_mean) ** 2
         squared_values_list1.append(value_squared1)
+
+    for item in squared_values_list1:
+        squared_values_list1_mean += item
+
+    squared_values_list1_mean /= len(squared_values_list1)
+
+    for entry in stock2:
+        stock2_mean_sum += entry
+        stock2_counter += 1
+
+    stock2_mean = stock2_mean_sum / stock2_counter
+
     for value in stock2:
         value_squared2 = (value - stock2_mean) ** 2
         squared_values_list2.append(value_squared2)
-    for item in squared_values_list1:
-        squared_values_list1_mean += item
-    squared_values_list1_mean / len(squared_values_list1)
-    std_dev_1 = math.sqrt(squared_values_list1_mean)
-    print(std_dev_1)
+
     for item in squared_values_list2:
         squared_values_list2_mean += item
-    squared_values_list2_mean / len(squared_values_list2)
-    std_dev_2 = math.sqrt(squared_values_list2_mean)
-    print(std_dev_2)
-    if std_dev_1 > std_dev_2:
-        print("Stock 1 has a higher standard deviation.")
-    elif std_dev_1 < std_dev_2:
-        print("Stock 2 has a higher standard deviation.")
-    elif std_dev_1 == std_dev_2:
-        print("The 2 stocks have the same standard deviation.")
-    else:
-        print("Error")
 
-compare_stocks(stock1, stock2)
+    squared_values_list2_mean /= len(squared_values_list1)
+
+    # The mean values within the squared values lists are square rooted and rounded to 6 decimal places, the latter
+    # is done in order to escape any possible floating-point errors.
+    std_dev_1 = round(math.sqrt(squared_values_list1_mean), 6)
+    std_dev_2 = round(math.sqrt(squared_values_list2_mean), 6)
+
+    if std_dev_1 > std_dev_2:
+        print("Stock 1 has a higher standard deviation, which is " + str(std_dev_1))
+    elif std_dev_1 < std_dev_2:
+        print("Stock 2 has a higher standard deviation, which is " + str(std_dev_2))
+    elif std_dev_1 == std_dev_2:
+        print("The 2 stocks have the same standard deviation, which is " + str(std_dev_1))
+    else:
+        raise ValueError("Error with output.")
+
+
+compare_two_stocks(first_stock, second_stock)
