a8242ea35fa09c239598c206474370af0828471e
Kyungho Jung <kyungho.jung@mail.utoronto.ca>
2014-12-01 20:55:27 -0500

    added/enhanced the test_cases

--- test_mining.py
+++ test_mining.py
@@ -2,19 +2,11 @@
 
 """ This  """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-
-__copyright__ = "2014 Susan Sim"
-__license__ = "MIT License"
-
-__status__ = "Prototype"
-
+__author__ = 'Kyungho Jung & Dimitar & Curtis'
 # imports one per line
 import pytest
 from mining import *
 from bonus_01_compare import *
-from bonus_02_visual import *
 
 
 def test_goog():
@@ -28,29 +20,33 @@ def test_goog():
 # tests that correct results are returned for TSE-SO stocks
 def test_tse():
     tse_stock = StockMiner('TSE-SO', 'data/TSE-SO.json')
-    assert six_best_months() ==
+    assert tse_stock.six_best_months() == [('2007/12', 20.98), ('2007/11', 20.89), ('2013/05', 19.96),
+                                           ('2013/06', 19.94), ('2013/04', 19.65), ('2007/10', 19.11)]
+    assert tse_stock.six_worst_months() == [('2009/03', 1.74), ('2008/11', 2.08), ('2008/12', 2.25),
+                                            ('2009/02', 2.41), ('2009/04', 2.75), ('2009/01', 3.14)]
 
-    assert six_worst_months() ==
 
 # Tests that mining throws the correct error when files are missing
 def test_files_presence():
     with pytest.raises(FileNotFoundError):
+        StockMiner('GOOG', "data/no_such_file.json")
+    with pytest.raises(FileNotFoundError):
         StockMiner('GOOG', "")
+
 
 # Tests that mining throws the correct error when files are of the wrong type (not .jsons)
 def test_file_types():
     with pytest.raises(TypeError):
         StockMiner('GOOG', "data/file.txt")
 
+
 # Tests that mining throws the correct error when .jsons are empty
 def test_enough_data():
     with pytest.raises(ValueError):
         StockMiner('GOOG', 'data/empty.json')
 
+
 # Tests that bonus_01_compare returns the correct values
 def test_bonus_01():
     assert compare_two_stocks("data/GOOG.json", "data/TSE-SO.json") == \
         "Stock 1 has a higher standard deviation, which is 143.6229"
-
-#Here is a bunch of stuff
-
